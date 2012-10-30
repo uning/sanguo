66cea020d92d550780fe6e2aebf012c2e9061cc3
playcrab@bw173 <playcrab@bw173>
2012-10-30 10:13:24 +0800

    增加s8

---
+++ roleconf/chat_server.s8_gamecomb.js
@@ -0,0 +1,21 @@
+var r = module.exports = {
+	 role: 'chat_server' // override by loader save from filename 
+	,confserver: 'mongodb://122.11.61.28:35050/nodeconfig'
+	,userMongo: 'mongodb://122.11.61.28:35050/chatserver'
+	,sessionRedis: 'redis://10.200.77.180:53000'
+	,chatRedis: 'redis://10.200.77.180:53005'
+	,msgMongo:'mongodb://10.200.77.181:35070/chatmsg'
+	,sec:'s7'
+	
+	,clearTimeOutUser:{
+		desc: '清除长时间没有连接的用户,周期'
+	    ,v:120
+	}
+	,heartbeats:400
+	,listenPort: 8008
+	,host: 's8.dzm.gamecomb.com'
+	,isDev: false
+	,log2console: true
+	,loglevel:'info'
+	,mefile: __filename // override by loader save use filename
+}
