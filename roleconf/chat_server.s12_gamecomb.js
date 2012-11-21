a6188b082d2e4dfb94f596290082ae838da3db1b
playcrab@bw173 <playcrab@bw173>
2012-11-21 12:28:45 +0800

    add

---
+++ roleconf/chat_server.s12_gamecomb.js
@@ -0,0 +1,21 @@
+var r = module.exports = {
+	 role: 'chat_server' // override by loader save from filename 
+	,confserver: 'mongodb://122.11.61.28:35050/nodeconfig'
+	,userMongo: 'mongodb://122.11.61.28:35050/chatserver'
+	,sessionRedis: 'redis://10.200.77.180:53000'
+	,chatRedis: 'redis://10.200.77.180:53005'
+	,msgMongo:'mongodb://10.200.31.72:35080/chatmsg'
+	,sec:'s12'
+	
+	,clearTimeOutUser:{
+		desc: '清除长时间没有连接的用户,周期'
+	    ,v:120
+	}
+	,heartbeats:400
+	,listenPort: 8012
+	,host: 's12.dzm.gamecomb.com'
+	,isDev: false
+	,log2console: true
+	,loglevel:'info'
+	,mefile: __filename // override by loader save use filename
+}
